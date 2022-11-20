ff20d4e780e610df864c5e603f90f231b5ede3ce
RiloArbabillah <rilo.arbabillah@gmail.com>
2022-11-21 00:18:21 +0800

    make required img local

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 const img = new Image();
-img.src = "https://i.ibb.co/Q9yv5Jk/flappy-bird-set.png";
+// img.src = "https://i.ibb.co/Q9yv5Jk/flappy-bird-set.png";
+img.src = "./sloppy-bird-set.png";
 
 // general settings
 let gamePlaying = false;
